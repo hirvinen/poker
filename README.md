106710029e3ad843b8979a9b9c0c9586c81ac9c2
Patrik Hirvinen <patrik.hirvinen@iki.fi>
2018-09-17 19:37:05 +0300

    Update README.md and about

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # poker
 A simple poker game in React Native Web
+A relatively current, possibly broken demo should be available at https://poker.hirvinen.fi/
--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ export default () => (
     <Layout title="About Quick Poker">
       <h1>About Quick Poker</h1>
       <p>This game is modelled after Pikapokeri by Veikkaus.</p>
+      <p>This is very much a work in progress and may be broken from time to time for now. See <a href="https://github.com/hirvinen/poker">source on github</a></p>
     </Layout>
   </div>
 )
