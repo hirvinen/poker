a326983c97eeec2b3703e9ad5e8b7bdc5d4d8712
Patrik Hirvinen <patrik.hirvinen@iki.fi>
2018-09-19 15:39:49 +0300

    Move deck creation and helpers to lib

--- components/classify.js
+++ components/classify.js
@@ -1,49 +1,14 @@
-// Helpers
-const Joker = {
-  suit : {
-    name : 'Joker',
-    short: 'J'
-  },
-  value : 0,
-  toString : () => ('🃏')
-}
-
-const hasJoker = (hand) => (hand.length > removeCard(hand, Joker).length)
-
-const sameValue = (a,b) => (a.value === b.value || (a.value * b.value === 0)) // UNUSED
-const sameSuit = (a,b) => (a.suit.short === b.suit.short || (a.value * b.value === 0))
-const cardEquals = (a,b) => (sameValue(a,b) && sameSuit(a,b)) // UNUSED
-
-const sameSuitCards = (deck, card) => 
-	deck.filter( c => sameSuit(c, card))
-
-const removeCard = (deck, card) =>
-	deck.filter( (c) =>
-		(c.suit !== card.suit) || (c.value !== card.value)
-	)
-
-const sortHand = (hand) => hand.sort((a,b) => {
-  if (a.value < b.value) { return -1 }
-  if (a.value > b.value) { return 1 }
-  return 0
-})
-
-const distinctValuesCount = (hand) => countValues(hand).filter((count, value) => count > 0).length
-
-const countValues = (hand) => {
-  const valueCounts = Array.from({length: 15}, () => 0)
-  hand.forEach(card => {
-      valueCounts[card.value]++
-    })
-    return valueCounts
-}
-
-const productOfCardCounts = (hand) => (
-  countValues(hand).filter( (count, value) => count > 0
-  ).reduce( (acc, curr) => curr * acc, 1)
-)
-
-// Classifiers
+import {Joker} from '../lib/deck'
+import {
+  hasJoker,
+  sameSuitCards,
+  removeCard,
+  sortHand,
+  distinctValuesCount,
+  countValues,
+  productOfCardCounts,
+  logDeck
+} from '../lib/helpers'
 
 const hasNOfAKind = (hand, n) => {
   if (n <= 1) {
@@ -143,9 +108,6 @@ const hasPair10ToAce = (hand) => {
   return hasNOfAKind(cards10ToAce, 2)
 }
 
-const logDeck = (deck, description='') => {
-  console.log(description, deck.map(card => card.toString()+`(${card.value})`))
-}
 
 const classify = (hand = []) => {
   if (!hand || hand.length < 5) {
